3949d1f1c6a055712146238b4c31bd119297dfe1
josnethmoreno <josnethmoreno@gmail.com>
2023-02-14 20:53:36 -0400

    shop grid

--- src/app/Shop.tsx
+++ src/app/Shop.tsx
@@ -1,7 +1,29 @@
 import style from './Shop.module.css'
 
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Shop() {
-  return (
-    <section className={style.shop}>Shop</section>
-  )
+	return (
+		<section className={style.shop}>
+			<div className={` ${style['shop-grid']}`}>
+				<div className={style['product']}>
+					<Link href='/product/1'>
+						<figure>
+							<Image src='/shop/1.png' alt='' fill={true} />
+						</figure>
+						<p className={style['product-title']}>BB 25th Anniversary Tee - $ 35.00</p>
+					</Link>
+				</div>
+				<div className={style['product']}>
+					<Link href='/product/2'>
+						<figure>
+							<Image src='/shop/2.png' alt='' fill={true} />
+						</figure>
+						<p className={style['product-title']}>BB 25th Anniversary Tee - $ 35.00</p>
+					</Link>
+				</div>
+			</div>
+		</section>
+	)
 }
